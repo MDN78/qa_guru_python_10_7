a2eb20da1d37b2878ccaacbbc2888cf8de86c911
Dimitri Maksimov <dmitry-maksimov@yandex.ru>
2024-02-01 22:49:37 +0300

    update conftest - fixture

--- tests/conftest.py
+++ tests/conftest.py
@@ -4,7 +4,7 @@ import pytest
 from utils.path import Path as P
 from utils.script_zip_unzip import add_files_to_zip, extract_files
 
-@pytest.fixture
+@pytest.fixture(scope='function', autouse=True)
 def directory_manager():
     if not os.path.exists(P.resources_directory):
         os.mkdir(P.resources_directory)
--- tests/test_arhive_files.py
+++ tests/test_arhive_files.py
@@ -3,19 +3,19 @@ from utils.path import Path as P
 from openpyxl import load_workbook
 import csv
 
-def test_read_pdf_file(directory_manager):
+def test_read_pdf_file():
     reader = pypdf.PdfReader(P.resources_directory + '/' + 'Python Testing with Pytest (Brian Okken).pdf')
     reader.pages[1].extract_text()
     assert "Python Testing with pytest" in reader.pages[1].extract_text()
 
-def test_read_xlsx_file(directory_manager):
+def test_read_xlsx_file():
     workbook = load_workbook(P.resources_directory + '/' + 'file_example_XLSX_50.xlsx')
     sheet = workbook.active
     assert sheet.cell(1, 2).value == 'First Name'
     assert sheet.cell(2, 2).value == 'Dulce'
     assert sheet.cell(2, 3).value == 'Abril'
 
-def test_read_csv_file(directory_manager):
+def test_read_csv_file():
     with open(P.resources_directory + '/' + 'catalog_auto_google.csv', 'r') as read:
         reader = csv.DictReader(read, delimiter=",")
         assert [row['id'] for row in reader] == ['K456653443']
